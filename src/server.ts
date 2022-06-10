99461bda1e83ab2fdb27837b83da7772634daf23
Rafael Aguiar <rafael.a1@hotmail.com>
2022-06-10 09:19:14 -0300

    Create Categories

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,8 @@
 import express, { response } from "express";
-
+import { categoriesRoutes } from "./routes/categories.routes";
 const app = express()
 
-app.get("/",(request, response) => {
-    return response.json({message: "Oi Rafael"})
-})
+app.use(express.json())
 
-
-app.post("/courses", (request, response) => {
-    const { name } = request.body;
-    return response.json ({ name })
-})
+app.use("/categories",categoriesRoutes)
 app.listen(3333, () => console.log("Server is running!"))
