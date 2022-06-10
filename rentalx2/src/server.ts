9c9ef261243d09d13874dfc83d70bf4e9f2708c8
Rafael Aguiar <rafael.a1@hotmail.com>
2022-06-10 13:36:57 -0300

    Utilizando o Princípio de Responsabilidade Única

--- rentalx2/src/server.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express()
-
-app.get("/",(request, response) => {
-    return response.json({message: "Oi Rafael"})
-})
-app.listen(3333, () => console.log("Server is running!"))
