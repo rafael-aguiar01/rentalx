562e3c0371880ebc8ff3c07a16718f60cba3d6dd
Rafael Aguiar <rafael.a1@hotmail.com>
2022-06-11 15:17:24 -0300

    Save date in repository

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,9 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoryController } from "./ImportCategoryController";
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
-const importCategoryUseCase = new ImportCategoryUseCase()
-const importCategoryController = new ImportCategoryController(importCategoryUseCase)
+const categoriesRepositoery = CategoriesRepository.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepositoery);
+const importCategoryController = new ImportCategoryController(importCategoryUseCase);
 
 export{importCategoryController}
