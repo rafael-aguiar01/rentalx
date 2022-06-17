5f69e88d3b6ab116bf47f61e36fa1fd745af2d18
Rafael Aguiar <rafael.a1@hotmail.com>
2022-06-17 15:29:53 -0300

    Specification Migration

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -13,17 +13,18 @@ class CreateSpecificationUseCase {
         private specificationsRepository: ISpecificationsRepository){
 
     }
-    execute({ name, description}: IRequest): void{
-        const specificationAlreadyExists = this.specificationsRepository.findByName(name)
+    async execute({ name, description}: IRequest): Promise<void>{
+        const specificationAlreadyExists = 
+        await this.specificationsRepository.findByName(name);
 
         if (specificationAlreadyExists){
-            throw new Error("Specification already exists!")
+            throw new Error("Specification already exists!");
         }
 
-       this.specificationsRepository.create({
+       await this.specificationsRepository.create({
         name,
         description
-       })
+       });
     } 
 }
 
