ed2cb59f209e6b833307ead8f009c05e7c0b8973
Rafael Aguiar <rafael.a1@hotmail.com>
2022-06-17 08:21:20 -0300

    Install e configure typeORM

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import express, { response } from "express";
 import swaggerUi from "swagger-ui-express";
 
-import { router } from './routes'
-import swaggerFile from './swagger.json'
+import { router } from './routes';
+import swaggerFile from './swagger.json';
 
-const app = express()
+import "./database";
 
-app.use(express.json())
+const app = express();
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3333, () => console.log("Server is running!"))
+app.listen(3333, () => console.log("Server is running!"));
