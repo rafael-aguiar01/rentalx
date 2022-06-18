c965778ebb4934b2114d2c02aaf0e8bf086104a4
Rafael Aguiar <rafael.a1@hotmail.com>
2022-06-18 10:31:56 -0300

    create user controller

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,9 +1,9 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDto";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
-
+@injectable()
 class CreateUserUseCase {
     
     constructor(
