b0e2c945dea4f5c61af93cb57a9c2259c1312e25
Rafael Aguiar <rafael.a1@hotmail.com>
2022-06-18 16:13:51 -0300

    creating exception handling

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../errors/AppError";
 import { NextFunction, Request, Response } from "express";
 import { verify  } from "jsonwebtoken";
 
@@ -12,7 +13,7 @@ export async function ensureAuthenticated(request: Request, response: Response, 
     const authHeader = request.headers.authorization;
 
     if(!authHeader){
-        throw new Error("Token missing");
+        throw new AppError("Token missing", 401);
     }
     
     //Bearer 9234fdafafdya7ad
@@ -28,12 +29,12 @@ export async function ensureAuthenticated(request: Request, response: Response, 
         const user = await usersRepository.findById(user_id);
 
         if(!user){
-            throw new Error("User does not exists")
+            throw new AppError("User does not exists", 401)
         }
         
         next();
     } catch {
-        throw new Error("Invalid token!");
+        throw new AppError("Invalid token!", 401);
         
     }
     
