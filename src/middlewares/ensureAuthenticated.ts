006c8907883b4ea99771afff04f7f96c3db86fa8
Rafael Aguiar <rafael.a1@hotmail.com>
2022-06-19 09:11:07 -0300

    Create Upload of Avatar

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -31,6 +31,10 @@ export async function ensureAuthenticated(request: Request, response: Response, 
         if(!user){
             throw new AppError("User does not exists", 401)
         }
+
+        request.user = {
+           id: user_id
+        }
         
         next();
     } catch {
--- src/modules/accounts/entities/User.ts
+++ src/modules/accounts/entities/User.ts
@@ -16,12 +16,15 @@ class User {
 
     @Column()
     password: string;
-
+    
     @Column()
     driver_license: string;
 
     @Column()
     isAdmin: boolean;
+    
+    @Column()
+    avatar: string;
     
     @Column()
     created_at: Date;
