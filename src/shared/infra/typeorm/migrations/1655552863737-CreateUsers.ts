d94ed95aa5755c0a02daa9f3bd9325ed297a13d0
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-03 15:20:05 -0300

    Create rental migration

--- src/shared/infra/typeorm/migrations/1655552863737-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1655552863737-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1655552863737 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",
