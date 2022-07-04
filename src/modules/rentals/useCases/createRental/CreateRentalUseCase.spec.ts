97278dfe8573a47ff0cbc34213bda8c445cbcdbc
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-04 08:09:40 -0300

    Create rental tests

---
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,21 @@
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { CreateRentalUseCase } from "./CreateRentalUseCase"
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+
+describe("Create Rental", () => {
+    beforeEach(() => {
+        rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+        createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+    })
+
+    it(" shoud be able to create a new rental", async() => {
+        await createRentalUseCase.execute({
+            user_id:"12343",
+            car_id: "12332123",
+            expected_return_date: new Date()
+        })
+    });
+
+});
