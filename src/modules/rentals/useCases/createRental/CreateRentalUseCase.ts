a7fb7271c9552bad613ac32af10d3a2f9d474609
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-04 11:34:56 -0300

    Create Controller rentall

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -3,6 +3,7 @@ import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
+import { injectable,inject } from "tsyringe";
 
 
 interface IRequest {
@@ -11,10 +12,13 @@ interface IRequest {
     expected_return_date: Date;
 }
 
+@injectable()
 class CreateRentalUseCase {
 
     constructor(
+        @inject("RentalsRepository")
         private rentalsRepository: IRentalsRepository,
+        @inject("DayjsDateProvider")
         private dateProvider: IDateProvider,
     ){}
     async execute({
---
+++ src/shared/container/providers/index.ts
@@ -0,0 +1,10 @@
+import { container } from 'tsyringe'
+
+import { IDateProvider } from './DateProvider/IDateProvider'
+import { DayjsDateProvider } from "./DateProvider/implementation/DayjsDateProvider"
+
+container.registerSingleton<IDateProvider>(
+    "DayjsDateProvider",
+    DayjsDateProvider
+)
+    
