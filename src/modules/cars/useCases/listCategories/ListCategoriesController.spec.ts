e6e814cea8caf931bcacfa49e785372bd92ac71e
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-05 08:34:57 -0300

    Create integration test

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -29,7 +29,7 @@ describe("Create Category Controller", () => {
         await connection.close()
     })
 
-    it("should be able to create a new category", async() => {
+    it("should be able to list all categories", async() => {
         const responseToken = await request(app).post("/sessions")
             .send({
                 email: "admin@rentx.com.br",
@@ -38,7 +38,7 @@ describe("Create Category Controller", () => {
 
         const { token } = responseToken.body;
         
-        const response = await request(app)
+        await request(app)
         .post("/categories")
         .send({
             name: "Category Supertest",
@@ -48,29 +48,14 @@ describe("Create Category Controller", () => {
             Authorization: `Bearer ${token}`
         });
 
-        expect(response.status).toBe(201);
+        const response = await request(app).get("/categories");
+
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(1);
+        expect(response.body[0]).toHaveProperty("id");
+        expect(response.body[0].name).toEqual("Category Supertest");
     });
 
-    it("should not be able to create a new category with name exists", async() => {
-        const responseToken = await request(app).post("/sessions")
-            .send({
-                email: "admin@rentx.com.br",
-                password: "admin"
-            })
-
-        const { token } = responseToken.body;
-        
-        const response = await request(app)
-        .post("/categories")
-        .send({
-            name: "Category Supertest",
-            description: "Category Supertest",
-        })
-        .set({
-            Authorization: `Bearer ${token}`
-        });
-
-        expect(response.status).toBe(400);
-    });
+    
 });
 
