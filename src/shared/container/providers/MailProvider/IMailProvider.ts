c7e9f7933b2d55c96947d4ff3503bc9dedcac582
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-11 10:17:13 -0300

    Insert Engine Template to send e-mail

--- src/shared/container/providers/MailProvider/IMailProvider.ts
+++ src/shared/container/providers/MailProvider/IMailProvider.ts
@@ -1,7 +1,12 @@
 
 
 interface IMailProvider {  
-    sendMail(to: string, subject: string, body: string): Promise<void>;
+    sendMail(
+        to: string, 
+        subject: string, 
+        variables: any, 
+        path: string
+    ): Promise<void>;
 }
 
 export { IMailProvider }
