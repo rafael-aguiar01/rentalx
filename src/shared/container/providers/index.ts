07b28acd743bb0241299ed79bce4a07ca0e2b8aa
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-12 15:43:10 -0300

    Create LocalStorageProvider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,8 @@ import { IDateProvider } from './DateProvider/IDateProvider'
 import { DayjsDateProvider } from "./DateProvider/implementation/DayjsDateProvider"
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
+import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
@@ -13,5 +15,10 @@ container.registerSingleton<IDateProvider>(
 container.registerInstance<IMailProvider>(
     "EtherealMailProvider",
     new EtherealMailProvider()
+);
+
+container.registerSingleton<IStorageProvider>(
+    "StorageProvider",
+    LocalStorageProvider
 );
     
