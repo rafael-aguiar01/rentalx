5a60f45c811346dd40a4c8b007c26ffa08426734
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-13 07:45:21 -0300

    Configure Upload to S3

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,6 +5,7 @@ import { DayjsDateProvider } from "./DateProvider/implementation/DayjsDateProvid
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
 import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
 import { IStorageProvider } from './StorageProvider/IStorageProvider';
 
 container.registerSingleton<IDateProvider>(
@@ -17,8 +18,13 @@ container.registerInstance<IMailProvider>(
     new EtherealMailProvider()
 );
 
+const diskstorage = {
+    local: LocalStorageProvider,
+    s3: S3StorageProvider,
+};
+
 container.registerSingleton<IStorageProvider>(
     "StorageProvider",
-    LocalStorageProvider
+    diskstorage[process.env.disk]
 );
     
