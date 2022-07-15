f9b6f3dcb06b8e5e90e985880cd040f624a772e4
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-15 12:30:34 -0300

    Configure Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,8 @@ import "dotenv/config";
 import express, { NextFunction, Response, Request } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import upload from '@config/upload';
 
 import cors from 'cors'
@@ -21,7 +23,21 @@ const app = express();
 
 app.use(rateLimiter);
 
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
+
+
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
@@ -30,6 +46,8 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler())
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
     if(err instanceof AppError){
