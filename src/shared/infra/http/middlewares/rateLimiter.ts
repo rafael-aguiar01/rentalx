2c19ddd5cd46c86f83d64b7ea0e07f53d479fc31
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-15 11:36:07 -0300

    Change redis version

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -1,36 +1,31 @@
 import { NextFunction, Request, Response } from "express";
 import { RateLimiterRedis } from "rate-limiter-flexible";
-import * as redis from "redis";
+import  redis from "redis";
+
 import { AppError } from "@shared/errors/AppError";
 
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+});
 
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "rateLimiter",
+  points: 5, // requests
+  duration: 5, // seconds
+});
 
 export default async function rateLimiter(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<void> {
-  const redisClient = redis.createClient({
-    legacyMode: true,
-    socket: {
-      host: process.env.REDIS_HOST,
-      port: Number(process.env.REDIS_PORT),
-    },
-  });
-
-  await redisClient.connect()
-  
-  const limiter = new RateLimiterRedis({
-    storeClient: redisClient,
-    keyPrefix: "rateLimiter",
-    points: 5,
-    duration: 5,
-  });
   try {
     await limiter.consume(request.ip);
     return next();
     
   } catch (err) {
-    throw new AppError(err.message, 429);
+    throw new AppError("Too many requests", 429);
   }
 }
