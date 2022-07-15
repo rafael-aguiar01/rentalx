0b8b7f3699d412c0f64eb8a219de772f7751840c
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-15 11:09:33 -0300

    Fix many connections

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -1,39 +1,36 @@
-import { NextFunction, Request, Response } from 'express';
-import { RateLimiterRedis } from 'rate-limiter-flexible';
-import {createClient} from 'redis';
+import { NextFunction, Request, Response } from "express";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import * as redis from "redis";
+import { AppError } from "@shared/errors/AppError";
 
-import { AppError } from '@shared/errors/AppError';
 
-const redisClient = createClient({
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  const redisClient = redis.createClient({
     legacyMode: true,
     socket: {
       host: process.env.REDIS_HOST,
       port: Number(process.env.REDIS_PORT),
     },
-});
+  });
 
-const limiter = new RateLimiterRedis({
+  await redisClient.connect()
+  
+  const limiter = new RateLimiterRedis({
     storeClient: redisClient,
     keyPrefix: "rateLimiter",
     points: 5,
     duration: 5,
-});
-
-export default async function rateLimiter(
-    request: Request,
-    response: Response,
-    next: NextFunction
-    ): Promise<void> {
-
-    try {
-        await limiter.consume(request.ip);
-
-        return next();
-    } catch(err){
-        throw new AppError("Too many requests", 429);
-    }
-        
-
-
+  });
+  try {
+    await limiter.consume(request.ip);
+    return next();
     
+  } catch (err) {
+    throw new AppError(err.message, 429);
+  }
 }
